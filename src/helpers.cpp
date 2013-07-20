5a2e5fb780a9dc833c54e411b60fea74bb9a80ef
LiquidHelium <andydjackson@gmail.com>
2013-07-20 19:43:09 +0100

    Included files accidnetally left out

---
+++ src/helpers.cpp
@@ -0,0 +1,24 @@
+#include "helpers.hpp"
+
+
+bool is_number(std::string string)
+{
+	if (string.size() < 1)
+		return false;
+		
+	for (char c : string)
+	{
+		if (!isdigit(c) && c != '.')
+			return false;
+	}
+
+	return true;
+}
+
+std::string read_file(std::string filename)
+{
+	std::ifstream t(filename.c_str());
+	std::stringstream buffer;
+	buffer << t.rdbuf();
+	return buffer.str();
+}
