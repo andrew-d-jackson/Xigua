f2eccd8ae7298c20cb326de90d6e70fe45d90a3c
LiquidHelium <andydjackson@gmail.com>
2013-07-28 18:23:29 +0100

    to-string method

--- src/functions/string.hpp
+++ src/functions/string.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <sstream>
 #include <functional>
 #include <limits>
 
@@ -15,6 +16,30 @@ namespace Xigua
 	{
 		namespace String
 		{
+
+
+			DataType to_string(std::vector<DataType> inputs, Enviroment* enviroment)
+			{
+				if (inputs.at(0).type() == DataTypes::String)
+					return inputs.at(0);
+
+				if (inputs.at(0).type() == DataTypes::Bool){
+					if (inputs.at(0).boolean())
+						return DataType(DataTypes::String, "true");
+					else
+						return DataType(DataTypes::String, "false");
+				}
+
+				if (inputs.at(0).type() == DataTypes::Number){
+					std::stringstream ss;
+					ss << inputs.at(0).number();
+					return DataType(DataTypes::String, ss.str());
+				}
+			
+				std::cout << "Incompatible Type in to string";
+				exit(1);
+			}
+
 
 			DataType concatinate(std::vector<DataType> inputs, Enviroment* enviroment)
 			{
