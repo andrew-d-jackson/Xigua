e465476f3d3bb1c59747864ce7d86ce25c4df583
LiquidHelium <andydjackson@gmail.com>
2014-02-07 08:59:34 +0000

    Basic REPL

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "enviroment.hpp"
 #include "stdlib.hpp"
@@ -6,30 +7,42 @@
 #include "error.hpp"
 #include "evaluate.hpp"
 
+#ifdef TEST
+#include "gtest/gtest.h"
+#endif
 
+int main(int argc, char *argv [])
+{ 
 #ifdef TEST
 
-#include "gtest/gtest.h"
-
-	int main(int argc, char *argv [])
-	{ 
-		::testing::InitGoogleTest(&argc, argv);
-  		return RUN_ALL_TESTS();
-	}
-
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 
 #else
 
-	int main(int argc, char *argv [])
-	{
+	if (argc > 1) {
 		xig::enviroment enviroment = xig::get_global_enviroment();
-
 		try {
 			xig::evaluate(enviroment, xig::parser::from_file(argv[1]));
 		} catch (xig::error e) {
 			e.print_default_message();
 		}
+	} else {
+		std::cout << "Xigua REPL" << std::endl;
+		xig::enviroment enviroment = xig::get_global_enviroment();
+		std::string input;
+		while (std::getline(std::cin,input)) {
+			if (input == "exit") break;
+			try {
+				std::cout << xig::string_representation(xig::evaluate(enviroment, xig::parser::from_string(input))) << std::endl;
+			} catch (xig::error e) {
+				e.print_default_message();
+			}
+		}
 		
+
 	}
+	
 
 #endif
+}
